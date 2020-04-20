9de8c546da6359dadcbd1d5f05180673db4193fa
Kirigaya Kazuto -ID <63974068+KIRIGAYA-KAZUT0@users.noreply.github.com>
2020-04-20 08:46:33 +0700

    Add files via upload

---
+++ kirito.py
@@ -0,0 +1,113 @@
+def salam(kata):
+     print (kata) 
+
+salam('\033[1;35m Link Start!!!')
+nama = input('\033[1;32m username : ')
+
+print ('\033[1;32m Welcome', nama)
+
+import requests,os
+os.system('clear')
+os.system ('toilet -f standard "K I R I T O" -F gay')
+print ("\033[1;36mAuthor:Kirigaya Kazuto")
+print ("\033[1;35mContact:085895742141")
+print ("\033[1;32m❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜")
+
+import requests, os
+def insta():
+    Print(f'[!] menginstall tools insta crack')
+    os.system('cd instacrack')
+    os.system('bash instacrack.sh')
+def hapus():
+ print(f'[!] Menginstall tools hapus data..')
+ os.system('cd hapusdata')
+ os.system('python hapusdata.py')
+ 
+def web():
+ print(f'[!] Mencari web vuln SQL..')
+ os.system('cd websql')
+ os.system('bash websql.sh')
+ 
+def cekip():
+ print(f'[!] Mendapatkan IP..')
+ re = requests.get('https://api.myip.com').json()
+ ip = re['ip']
+ print(f'[!] IP kamu : {ip}')
+ 
+def call():
+ print(f'[!] Menginstall tools spam call..')
+ os.system('cd callspam')
+ os.system('php callspam.php')
+ 
+def spamsms():
+ print(f'[!] Menginstall tools spam sms..')
+ os.system('cd spamsms')
+ os.system('php smstri.php')
+ 
+def mirror():
+ print(f'[!] Menginstall tools fake mirror..')
+ os.system('cd mirror')
+ os.system('bash mirror.sh')
+
+
+def kereta():
+ print(f'[!] Menginstall tools kereta api..')
+ os.system('pkg install sl')
+ os.system('sl')
+ os.system('sl')
+ os.system('sl')
+ os.system('sl&&sl&&sl&&sl&&sl&&sl')
+ 
+def view():
+ print(f'[!] Menginstall tools spam Visit web..')
+ os.system('cd visitor')
+ os.system('php visitor.php')
+
+def santet():
+ print(f'[!] Menginstall dukun sakti..')
+ os.system('cd santet')
+ os.system('python2 santet.py')
+
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + "\n":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.1)
+
+mengetik("\033[1;36m Selamat datang di isekai.Dibawah ini adalah beberapa item yang dapat kamu gunakan :\n ")
+
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+print ("\033[1;31m[\033[1;37m1\033[1;31m]\033[1;33m cek ip")
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+print ("\033[1;31m[\033[1;37m2\033[1;31m]\033[1;36m Spam Sms ")
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+print ("\033[1;31m[\033[1;37m3\033[1;31m]\033[1;33m Kereta api ")
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+print ("\033[1;31m[\033[1;37m4\033[1;31m]\033[1;36m Web vuln SQL")
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+print ("\033[1;31m[\033[1;37m5\033[1;31m]\033[1;33m spam visitor web")
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+print ("\033[1;31m[\033[1;37m5\033[1;31m]\033[1;36m Santet online")
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+print ("\033[1;31m[\033[1;37m6\033[1;31m]\033[1;33m Fake  Mirror Def.id")
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+print ("\033[1;31m[\033[1;37m7\033[1;31m]\033[1;36m insta crack")
+print("\033[1;32m╬╬═════════════════════════════╬╬")
+
+
+
+
+menu = input("\n\033[1;32mpilih item kamu: ")
+if menu =="0":hapus()
+elif menu == "1":cekip()
+elif menu == "2":spamsms()
+elif menu == "3":kereta()
+elif menu == "4":web()
+elif menu == "5":view()
+elif menu == "5":santet()
+elif menu == "6":mirror()
+elif menu == "7":insta()
