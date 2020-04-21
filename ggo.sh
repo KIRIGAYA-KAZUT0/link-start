b0c5236c63e01537667ad038826cbe74bfd22ca7
Kirigaya Kazuto -ID <63974068+KIRIGAYA-KAZUT0@users.noreply.github.com>
2020-04-21 12:30:57 +0700

    Add files via upload

---
+++ ggo.sh
@@ -0,0 +1,3 @@
+toilet -f standard "K I R I T O" -F gay
+echo -e '\e[1;35m ______________hack Facebook______________'
+echo -e '\e[1;36mContact :085895742141'
---
+++ sao.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib
+from multiprocessing.pool import ThreadPool
+
+#hay manusia udah nyampe di sini ea  ubah author ataupun ngerecode semoga emak bapaknya mati dalam keadaan mengenaskan
+#buat yg nyampe di sini cuman buat mempelajari pemrograman dan beberapa fungsinya ane ucapin selamat berjuang
+#tapi awaslu yg nge recode ataupun mengganti author
+def lodingcok():
+    looding2 = ['2','3','kontol','Saapii','Muukee','luuuuu','keeeek','BAABII','awokwokwok',]
+
+    for o in looding2:
+        print '\r\x1b[1;91m[wait] \x1b[1;92mLoading Tod.. \x1b[1;97m' + o,
+        sys.stdout.flush()
+        time.sleep(1.0)
+        
+        
+lodingcok()
