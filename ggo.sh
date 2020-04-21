a793ab7207c26d5bd0e0b19b5e96520d4a70460c
Kirigaya Kazuto -ID <63974068+KIRIGAYA-KAZUT0@users.noreply.github.com>
2020-04-21 13:10:26 +0700

    Add files via upload

--- ggo.sh
+++ ggo.sh
@@ -1,3 +1,14 @@
+
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+magenta='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+
+
+
 toilet -f standard "K I R I T O" -F gay
 echo -e '\e[1;35m ______________hack Facebook______________'
 echo -e '\e[1;36mContact :085895742141'
